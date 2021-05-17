600f8225156e713e1d2458d54eb9150003e4d616
omarr45 <58887202+omarr45@users.noreply.github.com>
2021-05-18 01:28:46 +0200

    added classNames for checboxes

---
+++ scripts/check.js
@@ -0,0 +1,15 @@
+// var panels = document.getElementsByClassName('para');
+// // console.log(panels);
+// // console.log('Started');
+
+// for (var i = 0; i < panels.length; i++) {
+//   panels.item(i).insertAdjacentHTML(
+//     'afterend',
+//     `<label class="custom-check">
+//       <input type="checkbox">
+//       <span class="checkmark"></span>
+//   </label>`
+//   );
+// }
+
+// // console.log('Done');
