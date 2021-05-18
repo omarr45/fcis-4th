647914ce4edf5ce13f5ac03bb4217e713e8f5ad3
omarr45 <58887202+omarr45@users.noreply.github.com>
2021-05-18 02:51:35 +0200

    Added CheckBoxes

--- scripts/check.js
+++ scripts/check.js
@@ -1,15 +1,34 @@
-// var panels = document.getElementsByClassName('para');
-// // console.log(panels);
-// // console.log('Started');
+let links = document.getElementsByClassName('addCheck');
 
-// for (var i = 0; i < panels.length; i++) {
-//   panels.item(i).insertAdjacentHTML(
-//     'afterend',
-//     `<label class="custom-check">
-//       <input type="checkbox">
-//       <span class="checkmark"></span>
-//   </label>`
-//   );
-// }
+for (let i = 0; i < links.length; i++) {
+  links.item(i).insertAdjacentHTML(
+    'afterend',
+    `<label class="custom-check">
+      <input type="checkbox" class="chk">
+      <span class="checkmark"></span>
+    </label>`
+  );
+}
 
-// // console.log('Done');
+let checkBoxes = document.getElementsByClassName('chk');
+
+for (let i = 0; i < checkBoxes.length; i++) {
+  const current = checkBoxes.item(i);
+  // Check if saved, load it
+  if (JSON.parse(localStorage.getItem(document.title + links.item(i).text))) {
+    current.checked = JSON.parse(
+      localStorage.getItem(document.title + links.item(i).text)
+    );
+  }
+  // else save it
+  else {
+    localStorage.setItem(document.title + links.item(i).text, current?.checked);
+  }
+}
+
+for (let i = 0; i < checkBoxes.length; i++) {
+  const current = checkBoxes.item(i);
+  current?.addEventListener('click', () => {
+    localStorage.setItem(document.title + links.item(i).text, current?.checked);
+  });
+}
